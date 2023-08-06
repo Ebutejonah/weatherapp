f3a73d62445eab2943c514b4af2980eefd9c0e03
giftedmatrix <ebutejonah386@gmail.com>
2023-08-06 05:31:22 +0100

    first commit

--- djweather/settings.py
+++ djweather/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b2p#b-$aa(#_jxv=duc=mgvc0@=@!mdlu#il-+$-dge=a-11!c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
