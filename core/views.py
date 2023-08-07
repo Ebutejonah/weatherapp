9ef871c7eae48eadd9b6cd246a6429a654421eb1
giftedmatrix <ebutejonah386@gmail.com>
2023-08-07 18:48:52 +0100

    config

--- core/views.py
+++ core/views.py
@@ -2,9 +2,10 @@ import datetime
 import requests
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from decouple import config
 
 def indexview(request):
-    API_KEY = open("API_KEY", "r").read()
+    API_KEY = config('API_KEY')
     current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
     forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
     if request.method == "POST":
