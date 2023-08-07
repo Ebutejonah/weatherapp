bd03c1e9cf22cc7b3a75e1e2ac251f24f78e0fc6
giftedmatrix <ebutejonah386@gmail.com>
2023-08-07 17:43:12 +0100

    first commit

--- djweather/urls.py
+++ djweather/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core.views import indexview
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indexview)
 ]
